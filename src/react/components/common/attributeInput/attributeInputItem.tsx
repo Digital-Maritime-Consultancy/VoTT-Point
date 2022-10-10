c7d4789cd0e570fe7fe83eceff4d48f4e64fb4c1
Jinki Jung <your.jinki.jung@gmail.com>
2022-10-10 11:03:48 +0200

    attribute input item format update

--- src/react/components/common/attributeInput/attributeInputItem.tsx
+++ src/react/components/common/attributeInput/attributeInputItem.tsx
@@ -15,20 +15,23 @@ export default class AttributeInputItem extends React.Component<IAttributeInputI
 
     public render() {
         return (
-            <div className="tag-item row">
-                <div className="col">
-                    <span className="p-2">{this.props.name}</span>
-                </div>
-                <div className="col">
-                    <input
-                        type="text"
-                        id={`attr-input-${this.props.name}`}
-                        key={this.props.name}
-                        placeholder={this.props.description}
-                        onChange={(e) =>
-                            this.props.onAttributesUpdated(this.props.name, e.currentTarget.value)
-                        }
-                    />
+            <div className="tag-item-block">
+                <div className="tag-content" style={{background: "rgb(27, 32, 36)"}}>
+                    <div>
+                        <span className="p-1">{this.props.name}</span>
+                    </div>
+                    <div>
+                        <input
+                            className="w-100 p-1"
+                            type="text"
+                            id={`attr-input-${this.props.name}`}
+                            key={this.props.name}
+                            placeholder={this.props.description}
+                            onChange={(e) =>
+                                this.props.onAttributesUpdated(this.props.name, e.currentTarget.value)
+                            }
+                        />
+                    </div>
                 </div>
             </div>
         );
